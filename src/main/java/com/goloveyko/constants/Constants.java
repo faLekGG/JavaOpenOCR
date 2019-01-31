f8f1b80444fb2b896fb17f87ff3e50381ff04c06
Vitali Halaveika <HalaveikaV@iba>
2019-01-31 10:47:13 +0300

    Changes to Constants

--- src/main/java/com/goloveyko/constants/Constants.java
+++ src/main/java/com/goloveyko/constants/Constants.java
@@ -5,10 +5,8 @@ public final class Constants {
 
     }
 
-    public static final String URI_JSON = "https://169.50.197.82:80/ocr";
-    public static final String URI_FILE = "https://169.50.197.82:80/ocr-file-upload";
-    /*public static final String URI_JSON = "YOUR_URL_FOR_REMOTE_UPLOAD";
-    public static final String URI_FILE = "YOUR_URL_FOR_FILE_UPLOAD";*/
+    public static final String URI_JSON = "YOUR_URL_FOR_REMOTE_UPLOAD";
+    public static final String URI_FILE = "YOUR_URL_FOR_FILE_UPLOAD";
     public static final String IMAGE_URL = "img_url";
     public static final String ENGINE = "engine";
     public static final String ENGINE_ARGS = "engine_args";
@@ -16,8 +14,7 @@ public final class Constants {
     public static final String LANGUAGE = "lang";
     public static final String INPLACE_DECODE = "inplace_decode";
     public static final String INPLACE_DECODE_VALUE = "false";
-    public static final String PATH_TO_FILE = "C:\\IBMProjects\\OpenOcrJava\\files\\";
-    //public static final String PATH_TO_FILE = "YOUR_PATH_TO_FILE";
+    public static final String PATH_TO_FILE = "YOUR_PATH_TO_FILE";
 
 
 }
